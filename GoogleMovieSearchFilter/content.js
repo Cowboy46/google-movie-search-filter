2ff38929cf3b45814a97e7aaae9ab0dfc3140961
John Daudelin <jed34@njit.edu>
2018-09-09 17:56:10 -0400

    Extract movie title from dom element, use it to filter movie results

--- GoogleMovieSearchFilter/content.js
+++ GoogleMovieSearchFilter/content.js
@@ -1,9 +1,14 @@
-//TODO: use min movie rating from options and api call for each movie to remove appropriate ones
-
-const movieList = $(".MiPcId.mlo-c")
+const movieList = $(".MiPcId.mlo-c");
 
 if (movieList.length > 0){
-    movieList[0].style.display = "none";
+    for (let i = 0; i < movieList.length; i++){
+        const title = movieList[i].childNodes[0].getAttribute("aria-label");
+
+        //TODO change below conditional to whether API call for iMDB movie rating returns lower rating than acceptable
+        if (title == "Moana" || title == "The Lion King"){
+            movieList[i].style.display = "none";
+        }
+    }
 }
 else {
     console.log("Couldn't find any google movie results...");
